b2828fb6b5922bde1802fa35ed7a2791bc81bcea
luz-garcia-garcia31 <Veronica.741247@gmail.com>
2019-05-21 14:51:02 -0500

    15

--- js/acciones.js
+++ js/acciones.js
@@ -1,29 +1,31 @@
-//JavaScript Documet
+//JavaScript Document
 
 $(document).ready(function(e){
-	document.addEventListener ("deviceready", onDeviceReady, false);
+	document.addEventListener ("deviceready",onDeviceReady,false);
 
 });
 
    function onDeviceReady(){
    
-   $('#posicion').on('click', function(){
+   $('#posicion').on('click',function(){
 	   getPosition();
 	   });
    }
    
    function getPosition(){
-	   var options={
+	   var options ={
 		   enableHighAccuracy :true,
 		   maximumAge: 3600000
 		   }
 	   
-	   var watchID = navigator.geolocation.getCurrentPosition(onSuccess, onError, options);
+	   var watchID = navigator.geolocation.getCurrentPosition(onSuccess,onError,options);
+	   
 	  function onSuccess(position){
+		  
 		  alert('Latitude:'   +position.coords.latitude   +'\n'+
 		  'Longitude:'       +position.coords.longitude    +'\n');
 		  };
 		 function onError(error){
-			 alert('code:'  +error.code  +'\n' + 'message:'  +error.message +'\n');
+			 alert('code:'  + error.code  +'\n' + 'message:'  + error.message +'\n');
 		}
 	   }
